79b458492d4abe971f69318e4aea2fc8edc1a87e
Emil Choo <164596180+ScarlettMoonn@users.noreply.github.com>
2024-04-16 13:54:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cre0gk406yzf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "emilchoo-employee"
+customregion = "us-east-1"
+
